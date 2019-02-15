6333ab4c73bb3296ace23c9bb13d40fc96d06d60
mmushko <mmushko@mmushko-ws-krakow.hw.griddynamics.net>
2019-02-15 16:49:34 +0100

    Create solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,28 @@ package com.company;
 
 public class Main {
 
+    public static double func(double x, double y){
+        return Math.pow(x,2)+Math.pow(y + Math.sqrt(x*x),2)-1;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+// write your code here
+
+        for(double x=-50; x<=50; x+=0.5){
+            for(double y=-50; y<=50; y+=0.5){
+                for(double i=-50; i<=50; i+=0.5){
+                    for(double j=-50; j<=50; j+=0.5) {
+                        if(i==func(x,y) && j==x){
+                            System.out.print(".");
+                        }
+
+                    }
+                }
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
     }
 }
